7ad1fab4a2b49e01e48efe1a0d50cc4c049260e2
Rita <ritaokonkwo6@gmail.com>
2020-02-08 13:20:13 +0100

    change image size and made menu and lesson screen

--- app/src/main/java/com/project/beaware/MenuActivity.java
+++ app/src/main/java/com/project/beaware/MenuActivity.java
@@ -1,8 +1,14 @@
 package com.project.beaware;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.list_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.list_menu){
+            Intent intent = new Intent(this, ListActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
