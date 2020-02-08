50e842730b56ece82185beadd99712af93b3cc99
Rita <ritaokonkwo6@gmail.com>
2020-02-08 11:32:36 +0100

    app screens

---
+++ app/src/main/java/com/project/beaware/CrimeActivity.java
@@ -0,0 +1,14 @@
+package com.project.beaware;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
