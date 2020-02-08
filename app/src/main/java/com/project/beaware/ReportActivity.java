d8a650dbc99574ed8963cafcc5a0fa6a6925f0fd
Opene Victoria <vickyella23@gmail.com>
2020-02-08 16:41:34 +0100

    reprt screen

--- app/src/main/java/com/project/beaware/ReportActivity.java
+++ app/src/main/java/com/project/beaware/ReportActivity.java
@@ -3,6 +3,8 @@ package com.project.beaware;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class ReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        String[] crimes = new String[]{"Drug Trafficking","Human Trafficking", "Fraud", "Kidnapping", "WildLike Poaching", "Sexual Harassment", "Illegal Trade", "Others"};
+
+        Spinner spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.spinner_list, crimes);
+        spinner.setAdapter(adapter);
+
+
     }
 }
